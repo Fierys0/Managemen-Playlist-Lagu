978885892e01409cd70ae94ebfd0b697b1c516bf
Strfrn <stefenshelinten27@gmail.com>
2026-05-28 21:08:52 +0700

    trying to add playlist card

--- src/states/mainMenu.cpp
+++ src/states/mainMenu.cpp
@@ -12,9 +12,14 @@ void MainMenu::Init() {
   settingBtn = Fumbo::UI::Button({50, 600, 75, 75});
   settingBtn.SetTexture(settingTex);
 
-  plusBtn = Fumbo::UI::Button({640, 360, 75, 75});
+  
+  plusBtn = Fumbo::UI::Button({603, 600, 75, 75});
   plusBtn.ApplyStyle(btnstyle);
   plusBtn.AddText("+", SpaceB, 80, currentTheme.second1);
+
+  Texture2D img1 = Fumbo::Assets::LoadTexture("assets/images/Cover.png");
+  playlistCards.emplace_back(Rectangle{460, 160, 360, 424}, img1, "My Playlist");
+  
 }
 void MainMenu::Cleanup() { UnloadTexture(settingTex); }
 void MainMenu::Update() {
@@ -24,13 +29,22 @@ void MainMenu::Update() {
 
   if (plusBtn.IsPressed())
     Fumbo::Instance().ChangeState(std::make_shared<AddPlaylist>());
+    for (auto& card : playlistCards) {
+    card.Update();
+    if (card.IsClicked()) {
+        // Play this playlist
+    }
 }
-void MainMenu::DrawClean() {}
+}
+void MainMenu::DrawClean() {} 
 void MainMenu::DrawDirty() {
   Fumbo::Graphic2D::DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(),
                                   currentTheme.prim1);
-  Fumbo::Graphic2D::DrawText("Hello World!", {100, 100}, SpaceB, 124,
-                             currentTheme.second1);
+  Fumbo::Graphic2D::DrawText("Hello World!", {50000, 100}, SpaceB, 124,
+                             currentTheme.second1);                         
   settingBtn.Draw();
   plusBtn.Draw();
+  for (auto& card : playlistCards) {
+    card.Draw();
+}
 }
--- src/states/mainMenu.hpp
+++ src/states/mainMenu.hpp
@@ -1,16 +1,20 @@
 #pragma once
 #include "fumbo.hpp"
+#include "playlistCard.hpp"
+#include <vector>
 
 class MainMenu : public IGameState {
 public:
-  void Init() override;
-  void Cleanup() override;
-  void Update() override;
-  void DrawClean() override;
-  void DrawDirty() override;
-
+    void Init() override;
+    void Cleanup() override;
+    void Update() override;
+    void DrawClean() override;
+    void DrawDirty() override;
+    
 private:
-  Fumbo::UI::Button settingBtn;
-  Fumbo::UI::Button plusBtn;
-  Texture2D settingTex;
+    Texture2D settingTex;
+    Fumbo::UI::Button settingBtn;
+    Fumbo::UI::Button plusBtn;
+    
+    std::vector<PlaylistCard> playlistCards;
 };
